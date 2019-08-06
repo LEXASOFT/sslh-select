2c2f801901bcd8dd373be69258ccc01a7b782edb
Paddy Xu <xupaddy@gmail.com>
2019-08-06 13:04:39 +0300

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,6 +2,7 @@
 
 sslh-select -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
    --ssh $SSH_HOST:$SSH_PORT \
+   --http $HTTP_HOST:$HTTP_PORT \
    --ssl $HTTPS_HOST:$HTTPS_PORT \
    --openvpn $OPENVPN_HOST:$OPENVPN_PORT \
    --anyprot $OTHER_HOST:$OTHER_PORT \
